f7c195c71c17e72eb2e4402cbef522ff56abb18d
mnemarius <marius_ne@outlook.com>
2026-01-31 10:49:03 +0100

    construction worker spawner

--- Assets/Scripts/ConstructionWorker/ConstructionWorker.cs
+++ Assets/Scripts/ConstructionWorker/ConstructionWorker.cs
@@ -2,18 +2,19 @@ using UnityEngine;
 
 public class ConstructionWorker : MonoBehaviour
 {
-
-    [SerializeField] private GameObject bus;
+    private GameObject bus;
     [SerializeField] private float speed = 5f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public void SetBusTarget(GameObject targetBus)
     {
-        
+        bus = targetBus;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (bus == null)
+            return;
+            
         transform.position = Vector3.MoveTowards(transform.position, bus.transform.position, speed * Time.deltaTime);
     }
 }
