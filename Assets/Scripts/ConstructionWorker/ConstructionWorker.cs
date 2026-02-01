31effd7af26fbf515a94ddd9ce0493bccc226b79
Hackerspace VR <hackerspacevr@gmail.com>
2026-02-01 13:10:28 +0100

    WIP tagger AI

--- Assets/Scripts/ConstructionWorker/ConstructionWorker.cs
+++ Assets/Scripts/ConstructionWorker/ConstructionWorker.cs
@@ -1,26 +1,47 @@
 using UnityEngine;
 using Combat;
+using UnityEngine.AI;
+using Unity.VisualScripting;
+using System;
 
 public class ConstructionWorker : MonoBehaviour
 {
+    public GameObject attackPoint;
+    public GameObject paintDecal;
+    public float attackCooldown = 2f;
+    public float paintSprayRadius = 1.5f;
+    public float tapeDamageReduction = 1f;
+    public float paintBaseDamage = 10f;
+
     private GameObject bus;
     [SerializeField] private float speed = 5f;
-    private CharacterController controller;
+    private float tryAttackRange = 20f;
+    private float attackRange = 5f;
+    private int bus_layer;
+    private int tape_layer;
+    private NavMeshAgent agent;
+    private float attackCooldownRemaining = 0f;
+    private float lastBusHitDistance = 0f;
+    private HealthComponent busHealth;
 
     private void Awake()
     {
-        controller = GetComponent<CharacterController>();
-
-        HealthComponent healthComponent = gameObject.GetComponent<HealthComponent>();
-        if (healthComponent != null)
+        agent = GetComponent<NavMeshAgent>();
+        if (!agent.isOnNavMesh)
         {
-            healthComponent.OnDeath += HandleDeath;
+            Destroy(gameObject);
+            return;
         }
+        HealthComponent healthComponent = gameObject.GetComponent<HealthComponent>();
+        healthComponent.OnDeath += HandleDeath;
+        bus_layer = 1 << LayerMask.NameToLayer("Bus");
+        tape_layer = 1 << LayerMask.NameToLayer("Tape");
     }
 
     public void SetBusTarget(GameObject targetBus)
     {
         bus = targetBus;
+        busHealth = bus.GetComponent<HealthComponent>();
     }
 
     private void HandleDeath()
@@ -33,14 +54,41 @@ public class ConstructionWorker : MonoBehaviour
         if (bus == null)
             return;
 
-        if (controller == null)
-            return;
+        if (attackCooldownRemaining > 0f)
+        {
+            attackCooldownRemaining -= Time.deltaTime;
+        }
 
-        // Calculate direction to bus
-        Vector3 direction = (bus.transform.position - transform.position).normalized;
-        
-        // Move towards bus
-        Vector3 movement = direction * speed * Time.deltaTime;
-        controller.Move(movement);
+        Vector3 busToTagger = transform.position - bus.transform.position;
+        agent.SetDestination(bus.transform.position + busToTagger.normalized * lastBusHitDistance);
+
+        if (attackCooldownRemaining <= 0f)
+        {
+            if (Vector3.Distance(transform.position, bus.transform.position) < tryAttackRange)
+            {
+                RaycastHit hit;
+                if (Physics.SphereCast(attackPoint.transform.position, .5f, attackPoint.transform.forward, out hit, attackRange, bus_layer))
+                {
+                    attackCooldownRemaining = attackCooldown;
+
+                    lastBusHitDistance = Vector3.Distance(hit.point, transform.position);
+                    Instantiate(paintDecal, bus.transform);
+                    paintDecal.transform.position = bus.transform.InverseTransformPoint(hit.point);
+
+                    float totalTapeLengthInArea = 0f;
+                    foreach (Collider col in Physics.OverlapSphere(hit.point, paintSprayRadius, tape_layer))
+                    {
+                        totalTapeLengthInArea += col.transform.localScale.z;
+                    }
+                    if (totalTapeLengthInArea < tapeDamageReduction)
+                    {
+                        float dmgReductionPercentage = totalTapeLengthInArea / tapeDamageReduction;
+                        int dmg = (int)Math.Ceiling(paintBaseDamage - paintBaseDamage * dmgReductionPercentage);
+                        //Debug.Log(dmg);
+                        busHealth.Damage(dmg);
+                    }
+                }
+            }
+        }
     }
 }
